82189c7e4252e31982f767ca45b7201d58fca338
Mauro Gestoso <maurogestoso@gmail.com>
2025-02-22 23:13:52 +0100

    Connect to remote db

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,24 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import db from "../db";
+
+export async function loader() {
+  const results = await db.execute(/* SQL */ `SELECT * FROM tools`);
+  return {tools: results.rows};
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Herramienteca" },
+    { name: "description", content: "Una biblioteca de herramientas para mi comunidad local" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function Home({loaderData}: Route.ComponentProps) {
+  const {tools} = loaderData;
+  return <>
+    <h1 className="font-bold text-3xl">Herramienteca</h1>
+    {tools.length ? <ul>{tools.map(tool => (
+      <li>{tool.name}</li>
+    ))}</ul>: <p>No tools found</p>}
+  </>;
 }
